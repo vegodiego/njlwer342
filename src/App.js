32f7dc5a879c684cead1a743ed2ec7c72a648996
vegodiego <53877309+vegodiego@users.noreply.github.com>
2020-04-14 18:11:45 -0500

    app ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react';
 
+
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: ['Sacar la ropa','Hacer la cama','Leer un rato'],
+      task: ""
+    }
+    this.change=this.change.bind(this);
+    this.add=this.add.bind(this);
+  }
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {this.state.list.map((task) =>
+              <li key={task}>{task}</li>
+            )}
           </ul>
            <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+             <input type="text" id="new-task" onChange={this.change} onKeyPress={this.add} value={this.state.task} placeholder="Ingresa una tarea y oprime Enter" />
            </form>
         </div>
       </div>
     )
+  }
+  change(event){
+    this.setState({
+      task:event.target.value
+    })
+  }
+
+  add(event){
+    if(event.key === "Enter"){
+      this.setState({
+        list: this.state.list.concat(this.state.task),
+        task: ""
+      })
+    }
   }
 }
 
